0fa668227f2ad5958679009d073515f34ed5f4d6
keshew <artyom.korotckov2017@yandex.ru>
2025-09-29 13:01:51 +0500

    Initial Commit

---
+++ MegaSlots/MegaSlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaSlotsApp.swift
+//  MegaSlots
+//
+//  Created by Артём Коротков on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MegaSlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
